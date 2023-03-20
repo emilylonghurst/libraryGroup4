cf06c7763e205bcc9414261e0e7729a7278d2745
Emily <123012648+emilylonghurst@users.noreply.github.com>
2023-03-20 18:10:58 +0000

    Update library.sql
    Changed description size to 'TEXT' to hold longer descriptions.

--- library.sql
+++ library.sql
@@ -9,7 +9,7 @@ CREATE TABLE books (
     author_name VARCHAR(255) NOT NULL,
     publication_date DATE NOT NULL,
     publisher 	VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
     PRIMARY KEY (book_id)
 );
 
